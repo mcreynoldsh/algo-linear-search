f318e3e9b636a6ec7d07197d6b364c56921367fc
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-24 15:27:28 -0500

    initials js solution for linear search

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -1,9 +1,24 @@
 const arrayToSearchThrough = [];
 for (let i = 1; i <= 1000; i++) {
-    arrayToSearchThrough.push(i);
+  arrayToSearchThrough.push(i);
 }
 
-exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+exports.linearSearch = function (valueToFind, arrayToSearchThrough) {
+  let index;
+  for (let x = 0; x < arrayToSearchThrough.length; x++) {
+    if (arrayToSearchThrough[x] === valueToFind) {
+      index = x;
+    }
+  }
+  return index;
 };
 
+exports.linearSearchGlobal = function (valueToFind, arrayToSearchThrough) {
+  let indices = [];
+  for (let x = 0; x < arrayToSearchThrough.length; x++) {
+    if (arrayToSearchThrough[x] === valueToFind) {
+      indices.push(x);
+    }
+  }
+  return indices;
+};
